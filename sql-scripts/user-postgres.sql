4d5f609eec1e5d0b77797888a9d4dd727f438bef
Aditya Bhonsle <aditya.bhonsle@lntinfotech.com>
2021-12-23 23:15:16 +0530

    Spring Security: User and Roles from JDBC

---
+++ sql-scripts/user-postgres.sql
@@ -0,0 +1,49 @@
+--
+-- Table structure for table users
+--
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    username varchar(50) NOT NULL,
+    password varchar(50) NOT NULL,
+    enabled int2 NOT NULL,
+    PRIMARY KEY (username)
+);
+
+--
+-- Inserting data for table users
+--
+
+INSERT INTO users
+VALUES
+    ('john','{noop}test123',1),
+    ('mary','{noop}test123',1),
+    ('susan','{noop}test123',1);
+
+
+--
+-- Table structure for table authorities
+--
+
+DROP TABLE IF EXISTS authorities;
+CREATE TABLE authorities
+(
+    username  varchar(50) NOT NULL,
+    authority varchar(50) NOT NULL,
+    UNIQUE (username,authority),
+    CONSTRAINT authorities_fk FOREIGN KEY (username) REFERENCES users (username)
+);
+
+--
+-- Inserting data for table authorities
+--
+
+INSERT INTO authorities
+VALUES
+    ('john','ROLE_EMPLOYEE'),
+    ('mary','ROLE_EMPLOYEE'),
+    ('mary','ROLE_MANAGER'),
+    ('susan','ROLE_EMPLOYEE'),
+    ('susan','ROLE_ADMIN');
+
+
